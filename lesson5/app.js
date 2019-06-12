08c829fc21686bc40af4a3b0dcfa776d93c898f3
Fliwat <574562727@qq.com>
2019-06-12 18:21:46 +0800

    lesson6 do half

--- lesson5/app.js
+++ lesson5/app.js
@@ -4,7 +4,7 @@ let concurrencyCount = 0
 let fetchUrl = function (url, callback) {
     let delay = parseInt((Math.random() * 10000000) % 2000, 10)
     concurrencyCount++
-    console.log(`下载的并发数是${concurrencyCount}, 正在抓取的是${url}, 耗时${delay}毫秒`)
+    console.log(`现在的并发数是${concurrencyCount}, 正在抓取的是${url}, 耗时${delay}毫秒`)
     setTimeout(() => {
         concurrencyCount--
         callback(null, `${url} html content`)
