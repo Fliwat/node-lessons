7c24fb9f8c4d9e22543c8ecc402124ccb79c53d4
Fliwat <574562727@qq.com>
2019-06-13 13:23:08 +0800

    lesson6 mocha & should & istanbul

--- lesson6/main.js
+++ lesson6/main.js
@@ -1,4 +1,13 @@
 function fibonacci(n) {
+    if (typeof n !== 'number') {
+        throw new Error('n should be a Number')
+    }
+    if (n < 0) {
+        throw new Error('n should >= 0')
+    }
+    if (n > 10) {
+        throw new Error('n should <= 10')
+    }
     if (n === 0 || n === 1) return n
     return fibonacci(n - 1) + fibonacci(n - 2)
 }
